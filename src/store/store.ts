bf5e22bb5a02e204a34de9defa7d96d6ddc3281d
Subham Goyal <subhamiitk@gmail.com>
2019-08-29 16:05:00 +0530

    Dispatching Actions

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,21 @@
 export class Store {
-    private subscribers: Function[];
-    private reducers: {[key: string]: Function};
-    private state: {[key: string]: any};
+  private subscribers: Function[];
+  private reducers: { [key: string]: Function };
+  private state: { [key: string]: any };
 
-    constructor(reducers = {}, initialState = {}) {
-        this.state = initialState;
-    }
+  constructor(reducers = {}, initialState = {}) {
+    this.state = initialState;
+  }
 
-    get value() {
-        return this.state;
-    }
+  get value() {
+    return this.state;
+  }
+
+  dispatch(action) {
+    this.state = {
+      ...this.state,
+      todos: [...this.state.todos, action.payload]
+    };
+    console.log(this.state);
+  }
 }
